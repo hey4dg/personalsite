fd8c241eceb151d96b2723e2a736729d649beaac
Emily Yun <Emily@Emilys-MacBook-Air-2.local>
2017-03-15 19:06:38 -0400

    website unresponsive

---
+++ main.js
@@ -0,0 +1,9 @@
+function scrollDown1() {
+	$('body').animate({ scrollTop: $('.portfolio').offset().top }, 1000);
+}
+
+
+function scrollDown2() {
+	$('body').animate({ scrollTop: $('.about').offset().top }, 1000);
+}
+
