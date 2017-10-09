d95c1e704a9b31e10c3b8fbd5f1ccbf23a14e02a
Hayoung Yun <hayoungyun@Hayoungs-MacBook-Pro.local>
2017-10-09 12:31:31 -0400

    P2

--- main.js
+++ main.js
@@ -1,132 +1,56 @@
 function scrollToTop() {
-	$('body').animate({ scrollTop: $('.top-container').offset().top }, 1000);
+	$('html, body').animate({ scrollTop: $('.top-container').offset().top }, 1500);
 }
 
+$('.logo').hover(function() {
+	$('.navhover').children('ul').stop(true, false, true).fade();
+	$('.logo').hover(function(e) {
+      	e.stopPropagation()});
+});
+
 function scrollDown1() {
-	$('body').animate({ scrollTop: $('.portfolio').offset().top }, 800);
+	$('html, body').animate({ scrollTop: $('.design').offset().top }, 1000);
 }
 
 
 function scrollDown2() {
-	$('body').animate({ scrollTop: $('.about').offset().top }, 1000);
+	$('html, body').animate({ scrollTop: $(".about").offset().top }, 1500);
 }
 
-function hide() {
-	document.getElementById('logo').style.display = 'none';
-	document.getElementById('dropdown').classList.toggle('show');
+function scrollDes() {
+	$('html, body').animate({ scrollTop: $("#design").offset().top }, 1000);
 }
 
-function show() {
-	document.getElementById('logo').style.display = 'block';
-}
-
-window.onclick = function(event) {
-	if (!event.target.matches('logo')) {
-		var dropdowns = document.getElementsByClassName("dropdown");
-		var i;
-		for (i = 0; i < dropdowns.length; i++) {
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
-	}
+function scrollArt() {
+	$('html, body').animate({ scrollTop: $(".art").offset().top }, 1000);
 }
 
 
-$('.contact-form').submit(function () {
-	var formData = $('.contact-form').serializeArray();
-	var errors = validateForm(formData);
+$(window).scroll(function() {
+	console.log($(window).scrollTop());
 
-	if (errors[0] != "") {
-		console.log("errors found");
-		$('.form_errors').html(errors[0]);
-		return false;
+	var des = document.getElementById('#design');
+	var pro = document.getElementById('#process');
+	var art = document.getElementById('#art');
+	var abo = document.getElementById('#about');
+
+	if ($(window).scrollTop() >= des - 15) {
+		$('.design').css('visibility', 'visible').hide().fadeIn(1150);
+		// $(this).off('scroll');
 	}
 
-	console.log("no errors, posting");
+	if ($(window).scrollTop() >= pro + 3) {
+		$('.process').css('visibility', 'visible').hide().fadeIn(1500);
+		// $(this).off('scroll');
+	}
 
-	$.post("send_form_email.php", formData);
-	triggerMessageSent();
-	
-	return false;
+	if ($(window).scrollTop() >= art + 3) {
+		$('.art').css('visibility', 'visible').hide().fadeIn(2000);
+		// $(this).off('scroll');
+	}
+
+	if ($(window).scrollTop() >= abo + 3) {
+		$('.about').css('visibility', 'visible').hide().fadeIn(2500);
+		$(this).off('scroll');
+	}
 });
-
-
-function triggerMessageSent() {
-	$('.form_wrapper').fadeOut(500, function() {
-		$('.form_wrapper').css('display', 'none');
-		$('.message_sent').fadeIn(750).css('display','block');
-	});
-}
-
-function validateForm(data) {
-
-	var numValues = 0;
-	for (var i=0; i<data.length; i++){
-		if (data[i]['value'] != "") numValues++;
-	}
-
-	if (numValues < 3) {
-		return ["Name, Email, and Message are required fields"];
-	}
-
-	var validName = validateName(escapeHTML(data[0]['value']));
-	var validEmail = validateEmail(escapeHTML(data[1]['value']));
-
-	var validPhone;
-	var message;
-	
-	if (numValues == 3){
-		if (data[2]['value'] != "") {
-			return ["Message field is required"];
-		} else {
-			message = escapeHTML(data[2]['value']);
-			validPhone = "";
-		}
-	} else {
-		validPhone = validatePhone(escapeHTML(data[2]['value']));
-		message = escapeHTML(data[3]['value']);
-	}
-
-	var errors = [];
-	var errorMessage = "";
-
-	if (!validName) errorMessage += 'Valid name required\n<br>';
-	if (!validEmail) errorMessage += 'Valid email required\n<br>';
-	if (!validPhone && validPhone != "") errorMessage += 'Valid phone required';
-
-	console.log("errorMessage", errorMessage);
-
-	errors.push(errorMessage);
-	console.log("errors", errors);
-
-	return errors;
-}
-
-function validateName(name) {
-	return /^[A-Za-z .'-]+$/.test(name);
-}
-
-function validateEmail(email) {
-	return /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/.test(email);
-}
-
-function validatePhone(phone) {
-	return /[^a-zA-Z.]/.test(phone);
-}
-
-function escapeHTML(text) {
-
-	text.trim();
-
-	var map = {
-	'&': '&amp;',
-	'<': '&lt;',
-	'>': '&gt;',
-	'"': '&quot;',
-	"'": '&#039;'
-	};
-
-	return text.replace(/[&<>"']/g, function(m) { return map[m]; });
-}
