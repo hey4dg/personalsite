7b3ef4580f5562470261692e874abedb9de88d07
Hayoung Yun <hayoungyun@Hayoungs-MacBook-Pro.local>
2018-03-31 14:32:05 -0400

    edit

--- main.js
+++ main.js
@@ -14,43 +14,59 @@ function scrollDown1() {
 
 
 function scrollDown2() {
-	$('html, body').animate({ scrollTop: $(".about").offset().top }, 1500);
+	$('html, body').animate({ scrollTop: $('.about').offset().top }, 1500);
 }
 
 function scrollDes() {
-	$('html, body').animate({ scrollTop: $("#design").offset().top }, 1000);
+	$('html, body').animate({ scrollTop: $('#design').offset().top }, 1000);
 }
 
 function scrollArt() {
-	$('html, body').animate({ scrollTop: $(".art").offset().top }, 1000);
+	$('html, body').animate({ scrollTop: $('.art').offset().top }, 1000);
 }
 
+// function process() {
+// 	$('html, body').animate({ scrollTop: $('proc').offset().top }, 1000);
+// 	var target = $(this.getAttribute('href'))
+// 	 $('html, body').stop().animate({
+//             scrollTop: target.offset().top
+//         }, 1000);
+// 		// $('html, body').animate({ scrollTop: $('#proc').offset().top }, 1000);
+// 	}
+// }
 
-$(window).scroll(function() {
-	console.log($(window).scrollTop());
+$(document).ready(function() {
+	$(".pro").click(function() {
+		$(".proc").fadeIn(500);
+	})
+})
 
-	var des = document.getElementById('#design');
-	var pro = document.getElementById('#process');
-	var art = document.getElementById('#art');
-	var abo = document.getElementById('#about');
 
-	if ($(window).scrollTop() >= des) {
-		$('.design').css('visibility', 'visible').hide().fadeIn(100);
-		// $(this).off('scroll');
-	}
+// $(window).scroll(function() {
+// 	console.log($(window).scrollTop());
 
-	if ($(window).scrollTop() >= pro + 3) {
-		$('.process').css('visibility', 'visible').hide().fadeIn(1500);
-		// $(this).off('scroll');
-	}
+// 	var des = document.getElementById('#design');
+// 	var pro = document.getElementById('#process');
+// 	var art = document.getElementById('#art');
+// 	var abo = document.getElementById('#about');
 
-	if ($(window).scrollTop() >= art + 3) {
-		$('.art').css('visibility', 'visible').hide().fadeIn(2000);
-		// $(this).off('scroll');
-	}
+// 	if ($(window).scrollTop() >= des) {
+// 		$('.design').css('visibility', 'visible').hide().fadeIn(100);
+// 		// $(this).off('scroll');
+// 	}
 
-	if ($(window).scrollTop() >= abo + 3) {
-		$('.about').css('visibility', 'visible').hide().fadeIn(2500);
-		$(this).off('scroll');
-	}
-});
+// 	if ($(window).scrollTop() >= pro + 3) {
+// 		$('.process').css('visibility', 'visible').hide().fadeIn(1500);
+// 		// $(this).off('scroll');
+// 	}
+
+// 	if ($(window).scrollTop() >= art + 3) {
+// 		$('.art').css('visibility', 'visible').hide().fadeIn(2000);
+// 		// $(this).off('scroll');
+// 	}
+
+// 	if ($(window).scrollTop() >= abo + 3) {
+// 		$('.about').css('visibility', 'visible').hide().fadeIn(2500);
+// 		$(this).off('scroll');
+// 	}
+// });
